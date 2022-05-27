c44980c0da218e9e7c11cc11982f30c7bb6e9136
rayback00001 <rabrosrajgopu@gmail.com>
2022-05-27 21:55:50 +0200

    new

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
 var typed = new Typed(".input", {
-    strings:["Designer", "Developer", ""],
+    strings:["Designer", "Developer", "cooler Typ"],
     typeSpeed:120,
     backSpeed:110,
     loop: true
